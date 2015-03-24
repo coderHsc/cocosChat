28ccfaba170c7d6d397e2af36eadfbd4420eee64
coderHsc <18386979@qq.com>
2015-03-24 16:12:01 +0800

    Update socket_manager.lua

--- src/app/scenes/socket_manager.lua
+++ src/app/scenes/socket_manager.lua
@@ -7,10 +7,9 @@ SocketManager = class("SocketManager")
 
 self = SocketManager
 
-
 function SocketManager:init()
 	if not self._socket then
-        self._socket = cc.net.SocketTCP.new("192.168.38.21", 8888, false)
+        self._socket = cc.net.SocketTCP.new("192.168.38.92", 8888, false)
         self._socket:addEventListener(cc.net.SocketTCP.EVENT_CONNECTED, handler(self, self.onStatus))
         self._socket:addEventListener(cc.net.SocketTCP.EVENT_CLOSE, handler(self,self.onStatus))
         self._socket:addEventListener(cc.net.SocketTCP.EVENT_CLOSED, handler(self,self.onStatus))
@@ -19,26 +18,17 @@ function SocketManager:init()
     end
     self._socket:connect()
 end
-
+local cnt = 0
+local session = 0
 function SocketManager:onData(__event)
-   
+
+
     local data = MessageManager:unpack_package(__event.data)
     MessageManager:print_package(data)
-   -- print("onData",data)
 
-
-
-    --session = session + 1
-    --local str = request("get",{ what = "hello" }, session)
-    --local size = #str
-    local data = MessageManager:pack_package("str")
-
-    --local pack = string.char(extract(size,8,8)) .. string.char(extract(size,0,8)) .. str
-
-
-    -- print("pack",pack,size,#pack)
-  
-    self._socket:send(data)
+    session = session + 1
+    local pack  = MessageManager:pack_package("get",{ what = "hello" },session)
+    self._socket:send(pack)
 
 end
 
